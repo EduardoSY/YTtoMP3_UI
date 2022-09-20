399fc1d571ecca32fb8830222111166a211d4aa4
Eduardo Da Silva <alu0101104911@ull.edu.es>
2022-09-20 19:19:43 +0100

    Updated GUI using TkDesigner

--- main.py
+++ main.py
@@ -7,22 +7,32 @@ import os
 from moviepy.editor import *
 
 downloadpath = os.getcwd()
+
 ventana = tk.Tk()
 
 def yt_mp3_download(url):
+    entry1.delete(0, 'end')
+    info = pytube.YouTube(url)
+    nombreCancion.config(text = str("Título del video:\n" + info.title))
+    duracionCancion.config(text = str("Duración: " + str(info.length//60) +":"+str("{:02d}".format(info.length%60))))
+    
+    download_button = tk.Button(
+    ventana,
+    text='Download',
+    command = lambda: download_process(url)
+    )
+    download_button.pack()
+    
+    
+def download_process(url):
     yt = pytube.YouTube(url)
     nombreCancion.config(text = str("Título del video:\n" + yt.title))
     duracionCancion.config(text = str("Duración: " + str(yt.length//60) +":"+str("{:02d}".format(yt.length%60))))
     print(downloadpath)
     archivo_descarga = yt.streams.get_audio_only().download(output_path=downloadpath)
-    #print("ITAG usado: ")
-    #print(yt.streams.get_audio_only())
-    #print(*yt.streams, sep='\n')
-
     audioclip = AudioFileClip(archivo_descarga)
     audioclip.write_audiofile(audioclip.filename.replace('.mp4', '.mp3'), bitrate='128k')
-    #os.remove(archivo_descarga)
-
+    os.remove(archivo_descarga)
 
 def setDownloadPath():
     pathDescarga = filedialog.askdirectory()
@@ -33,6 +43,7 @@ def setDownloadPath():
 
 ventana.title('Youtube to Mp3')
 ventana.geometry('300x200')
+
 # Paso 1. Seleccionar ruta
 tk.Label(ventana, text='1. Selecciona la ruta de descarga').pack()
 pathlabel = tk.Label(ventana, text= downloadpath)
@@ -47,14 +58,14 @@ path_button.pack()
 #Paso 2. Poner URL
 tk.Label(ventana, text='URL').pack()
 entry1 = tk.Entry(ventana)
-entry1.pack()
+entry1.pack(expand=True)
 
-download_button = tk.Button(
+search_button = tk.Button(
     ventana,
-    text='Download',
+    text='Search',
     command = lambda: yt_mp3_download(entry1.get())
 )
-download_button.pack()
+search_button.pack()
 
 #3. Informacion de la cancion
 nombreCancion = tk.Label(ventana, text='')
@@ -62,8 +73,4 @@ nombreCancion.pack()
 duracionCancion = tk.Label(ventana, text='')
 duracionCancion.pack()
 
-
-
-
 ventana.mainloop() #Necesario para que la ventana aparezca y se mantenga abierta
-
